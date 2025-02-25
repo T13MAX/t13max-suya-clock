e7dcfa6557c66a146f91e6a3c0a713188c797cba
t13max@163.com <t13max@163.com>
2025-02-25 21:08:36 +0800

    启动脚本修改

--- suya-clock/start.bat
+++ suya-clock/start.bat
@@ -10,7 +10,7 @@ set MAX_MEMORY=-Xmx128m
 
 set SCRIPT_DIR=%~dp0
 
-set JRE_PATH=%SCRIPT_DIR%jdk/jdk-win/bin/java.exe
+set JRE_PATH=%SCRIPT_DIR%jdk/jdk-win/bin/javaw.exe
 
 set LIBS_PATH=%SCRIPT_DIR%libs
 
@@ -18,6 +18,6 @@ set MAIN_CLASS=com.t13max.suyaclock.SuyaClockApplication
 
 set CLASSPATH=%LIBS_PATH%/*
 
-%JRE_PATH% -DSCRIPT_DIR=%SCRIPT_DIR% %INITIAL_MEMORY% %MAX_MEMORY% -cp %CLASSPATH% %MAIN_CLASS% -outputEncoding utf-8
+start %JRE_PATH% -DSCRIPT_DIR=%SCRIPT_DIR% %INITIAL_MEMORY% %MAX_MEMORY% -cp %CLASSPATH% %MAIN_CLASS% -outputEncoding utf-8
 
 endlocal
